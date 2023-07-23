d34a0f3f5104bf86e41ac6fd4a31d816e08d0ada
Wesley Schuab <wesley.schuab@p42.com.br>
2023-07-23 17:26:55 -0300

    a variavel long

--- CriandoVariaveis/Program.cs
+++ CriandoVariaveis/Program.cs
@@ -6,10 +6,16 @@ class Programa
         Console.WriteLine("Criando Variaveis");
         int idade;
         idade = 27;
+
         double salario;
         salario = 3000.10;
+
         int salarioInteiro;
         salarioInteiro = (int)salario;
+
+        Console.WriteLine(  " Valores maiores que 32b");
+        long x;
+        x = 2000000000000000000;
 
         Console.WriteLine("Tecle enter para fechar");
         Console.ReadLine();
