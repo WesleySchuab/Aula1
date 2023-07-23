ffdeab86d9dd97341b19310d97612d155dbcbc8d
Wesley Schuab <wesley.schuab@p42.com.br>
2023-07-23 17:16:01 -0300

    criando variavel double

--- CriandoVariaveis/Program.cs
+++ CriandoVariaveis/Program.cs
@@ -6,6 +6,8 @@ class Programa
         Console.WriteLine("Criando Variaveis");
         int idade;
         idade = 27;
+        double salario;
+        salario = 3000.10
 
         Console.WriteLine("Tecle enter para fechar");
         Console.ReadLine();
