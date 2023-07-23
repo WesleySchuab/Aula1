669f0f831adb068a9848910e940b9f09ed2bc0ea
Wesley Schuab <wesley.schuab@p42.com.br>
2023-07-23 18:51:15 -0300

    Decisao com if

---
+++ Decisao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 7 - Condicionais");
+
+        Console.WriteLine("Executando o projeto 7 - Condicionais");
+
+        int idadeJoao = 16;
+
+        if (idadeJoao >= 18)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+
+}
