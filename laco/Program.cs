cd2683c587b72af6b523097f9da8450e01fa2f38
Wesley Schuab <wesley.schuab@p42.com.br>
2023-07-28 14:09:19 -0300

    Laço de repetição

---
+++ laco/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 10 - Calcula Poupança");
+
+        double investimento = 1000;
+
+        int mes = 1;
+    }
+}
