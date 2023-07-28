73e164264b7c4ccda7a27bb1c08bddce91f02011
Wesley Schuab <wesley.schuab@p42.com.br>
2023-07-28 14:40:13 -0300

    Encadeamento de for

--- laco/Program.cs
+++ laco/Program.cs
@@ -5,22 +5,42 @@ class Programa
     {
         Console.WriteLine("Executando o projeto 10 - Calcula Poupança");
 
+        /*double investimento = 1000;
+
+         int mes = 1;
+         while (mes <= 12)
+         {
+             investimento = investimento + investimento * 0.005;
+             Console.WriteLine("No mês " + mes + " você tem R$" + investimento);
+
+             mes = mes + 1;
+         }
+
+         Console.WriteLine( "Usando laço For");
+         for (int mes = 1; mes <= 12; mes++)
+         {
+             investimento *= 1.005;
+             Console.WriteLine("No mês " + mes + " você tem R$ " + investimento);
+         }   
+        */
+        Console.WriteLine("Executando o projeto 12 - Investindo a Longo prazo");
+
+        double fatorRendimento = 1.005;
         double investimento = 1000;
 
-       /* int mes = 1;
-        while (mes <= 12)
+        for (int anos = 1; anos <= 5; anos++)
         {
-            investimento = investimento + investimento * 0.005;
-            Console.WriteLine("No mês " + mes + " você tem R$" + investimento);
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                investimento *= fatorRendimento;
+            }
 
-            mes = mes + 1;
+            fatorRendimento += 0.001;
         }
-       */
-        Console.WriteLine( "Usando laço For");
-        for (int mes = 1; mes <= 12; mes++)
-        {
-            investimento *= 1.005;
-            Console.WriteLine("No mês " + mes + " você tem R$ " + investimento);
-        }   
+
+        Console.WriteLine("Depois de 5 anos você terá R$ " + investimento);
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
     }
 }
